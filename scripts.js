140a0596829e1a0f5c87ad5d76d479cc4c5e7613
Jack Ceballos <jackrceballos@gmail.com>
2020-10-24 16:40:54 -0400

    Finished making questions arrays.

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// List of questions
+var questionArr = [
+    {
+        question: "What does HTML stand for?",
+        answerChoices: ["Hype Tool Making Language", "How To Make Language", "Hyper Text Markup Language", "Home Text Marker Language"],
+        answer: "Hyper Text Markup Language",
+    },
+    {
+        question: "Which set is an Array?",
+        answerChoices: ["{1,2,3,4}", "[1,2,3,4]", "(1,2,3,4)", "|1,2,3,4|"],
+        answer: "[1,2,3,4]",
+    },
+    {
+        question: "How do you make a comment in CSS?",
+        answerChoices: ["/$comment$/", "//comment//", "/!comment!/", "/*comment*/"],
+        answer: "/*comment*/",
+    },
+    {
+        question: "Which language does jQuery use?",
+        answerChoices: ["JavaScript", "HTML", "CSS", "C++"],
+        answer: "JavaScript",
+    },
+    {
+        question: "Which of the following is not a data type?",
+        answerChoices: ["Strings", "Methods", "Numbers", "Booleans"],
+        answer: "Methods",
+    }
+];
